065fba18eab554eb2a685f1e6f1c342da117b6cb
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-07-30 20:50:33 -0400

    updated init

--- project/__init__.py
+++ project/__init__.py
@@ -5,11 +5,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
+app.config.from_pyfile('config.py')
 config_path = os.environ.get("CONFIG_PATH", "project.config.DevelopmentConfig")
 app.config.from_object(config_path)
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
 
 from database import Base, engine
 Base.metadata.create_all(engine)
